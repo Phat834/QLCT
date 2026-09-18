19801ee5afab744b8874f7df22c9ac44a3eccd22
Tấn Phát <huynhtanphat080304@gmail.com>
2026-09-18 23:12:28 +0700

    fix dupilicate

--- fe/src/pages/BudgetList.tsx
+++ fe/src/pages/BudgetList.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useApp } from '../contexts/AppContext';
 import { api } from '../services/api';
-import { api } from '../services/api';
+
 import CurrencyInput from '../components/CurrencyInput';
 import { parseCurrency, formatCurrency } from '../utils/currency';
 import { Plus, Pencil, Trash2, X, Wallet, AlertTriangle, Calendar } from 'lucide-react';
